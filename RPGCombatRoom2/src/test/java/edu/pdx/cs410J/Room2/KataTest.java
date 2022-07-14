12dc9c3d930e75e959c6955ee278481adb5b3096
David Whitlock <david.m.whitlock@gmail.com>
2022-07-13 21:04:01 -0700

    Added empty kata projects for the Mob Programming katas

---
+++ RPGCombatRoom2/src/test/java/edu/pdx/cs410J/Room2/KataTest.java
@@ -0,0 +1,13 @@
+package edu.pdx.cs410J.Room2;
+
+import org.junit.jupiter.api.Test;
+
+public class KataTest
+{
+
+  @Test
+  void canInstantiateKataClass() {
+    new Kata();
+  }
+
+}
---
+++ RPNCalculatorRoom6/src/test/java/edu/pdx/cs410J/Room6/KataTest.java
@@ -0,0 +1,13 @@
+package edu.pdx.cs410J.Room6;
+
+import org.junit.jupiter.api.Test;
+
+public class KataTest
+{
+
+  @Test
+  void canInstantiateKataClass() {
+    new Kata();
+  }
+
+}
